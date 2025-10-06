acac76e57080618a6ded8f03d0c08c8ec820083f
Thar <user@Thar.local>
2025-10-06 13:35:21 +0700

    tested by crating model and section classes

--- test.py
+++ test.py
@@ -1,66 +1,33 @@
-from openai import OpenAI
-import uuid #generate unique identifier for each tutoring session
-
-# Initialize the OpenAI client
-client = OpenAI(api_key="deepseek_api_key", base_url="https://api.deepseek.com")
-
-chat_sessions={}
+from services.tutor_service import TutorService
 
 
-def read_system_prompt(file_path):
-    try:
-        
-        with open(file_path, 'r') as f:
-            return f.read()
-    except Exception as e:
-            print("Can't open the file because of ", e)
-            return " You are a good teacher assistant."
-        
-prompt=read_system_prompt('./system_prompt.txt')
-
-system_prompt = {
-    "role": "system",
-    "content": prompt
-}
+tutor_service = TutorService()
 
 
-def generate_chat_id():
-    id=str(uuid.uuid4())
-    chat_sessions[id]=[]
-    chat_sessions[id].append(system_prompt)
-    return id
-
-def send_message(chat_id, user_message):
-    if chat_id not in chat_sessions:
-        raise ValueError("Chat session not found!")
-    
-   
-    chat_sessions[chat_id].append({"role": "user", "content": user_message})
- 
-    response = client.chat.completions.create(
-        model="deepseek-ai/DeepSeek-V3",
-        messages=chat_sessions[chat_id]
-    ).choices[0].message.content.strip()
- 
-    chat_sessions[chat_id].append({"role": "assistant", "content": response})
-    
-    return response
+student_id = "student123"
 
 
-first_chat=generate_chat_id()
+session_id = tutor_service.create_session(student_id)
+print(f"Tutoring session created with ID: {session_id}")
 
-print("Response1 : ", send_message(first_chat, "What are the benefits of your product?"))
+student_query = "In what year Rome burned down?"
+print(f"Student Query: {student_query}")
 
-print("Response2 : ", send_message(first_chat, "Any other benefit?"))
+try:
+    ai_response = tutor_service.process_query(student_id, session_id, student_query)
+    print(f"AI Response: {ai_response}")
+except Exception as e:
+    print(f"Error: {e}")
 
 
-second_chat=generate_chat_id()
 
-print("Response1 : ", send_message(second_chat, "What are the cons of your product?"))
+session2=tutor_service.create_session(student_id)
 
-print("Response2 : ", send_message(first_chat, "Do you have solutions for them?"))
+query2="In what year Korean became modern?"
+print(f"session 2 Query: {query2}")
 
-for s in chat_sessions:
-    print(f"Session id : {s}")
-    for m in chat_sessions[s]:
-        print(m["role"], ":", m["content"])
+try:
+    reply=tutor_service.process_query(student_id, session2, query2)
+    print(f"session 2 reply: {reply}")
+except Exception as e:
+    print(f"Error: {e}")

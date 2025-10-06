de6ae00ee60b8b82bc24fea352c5fa8fdb41df3e
Thar <user@Thar.local>
2025-10-06 09:58:00 +0700

    tested system prompt

--- test.py
+++ test.py
@@ -3,14 +3,19 @@ from openai import OpenAI
 # Initialize the OpenAI client
 client = OpenAI(api_key="deepseek_api_key", base_url="https://api.deepseek.com")
 
-#simple prompt
-prompt = "Can you tell me an engineering joke?"
+
+system_prompt = "You are a skillful software engineer who ca solve all bugs and errors."
+conversation = [
+    {"role": "system", "content": system_prompt},
+    {"role": "user", "content": "What's your favorite type of pizza?"},
+]
+
 
 response = client.chat.completions.create(
     model="deepseek-ai/DeepSeek-V3",
-    messages=[{"role": "user", "content": prompt}],
-    max_tokens=100 #limit the response whether it finishes or not
-    temperature=0.2 #low temperature -> precise, high temperature->creative
+    messages=conversation,
+    max_tokens=200, #limit the response whether it finishes or not
+    temperature=0.2, #low temperature -> precise, high temperature->creative
     presence_penalty = 0.8, #higher value penaltizes the repeating words in conversation
     frequency_penalty=0.9,#higher value penaltizes the repeating words in response
 ) 
@@ -18,6 +23,4 @@ response = client.chat.completions.create(
 # Extract response from  API result
 reply = response.choices[0].message.content.strip()
 
-# Display the results
-print("Query:", prompt)
 print("Answer:", reply)
